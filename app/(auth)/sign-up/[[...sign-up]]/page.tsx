9fea4c17119ebaa7ff0c6f10d5a31044d5c37a11
dikeshchouhan1 <dikeshchouhan02@gmail.com>
2024-09-16 17:59:48 +0530

    UI change

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,10 +1,160 @@
 import { SignUp } from '@clerk/nextjs'
+import Image  from 'next/image'
 
 const SignUpPage = () => {
   return (
+    <div>
+    <div>
+    
+
+<div className="bg-blue flex items-center justify-center">
+<div className="mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8">
+  <div className="relative isolate overflow-hidden bg-gray-900 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
+    <svg viewBox="0 0 1024 1024" className="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0" aria-hidden="true">
+      <circle cx="512" cy="512" r="512" fill="url(#759c1415-0410-454c-8f7c-9a820de03641)" fill-opacity="0.7" />
+      <defs>
+        <radialGradient id="759c1415-0410-454c-8f7c-9a820de03641">
+          <stop stop-color="#7775D6" />
+          <stop offset="1" stop-color="#E935C1" />
+        </radialGradient>
+      </defs>
+    </svg>
+    <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
+      <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">Boost your productivity.
+        
+        Start using CodeDocs app today.</h2>
+      <p className="mt-6 text-lg leading-8 text-gray-300"></p>
+      <div className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
+        <a href="#" className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white">Get started</a>
+        <a href="#" className="text-sm font-semibold leading-6 text-white">Learn more <span aria-hidden="true">→</span></a>
+      </div>
+    </div>
+    <div className="relative wt-100 h-110 lg:mt-6">
+    <div> <div className="w-full pt-10">
+  <main role="main" className="w-full flex flex-col  justify-center">
+      <div className="w-full  pt-28 ">
+          <div className="bg-white rounded shadow px-1 pt-1 pb-1 sm:p-8 sm:pb-9">
+            <Image 
+
+        src="/assets/images/codedoc.png"
+        alt="Logo with name"
+        width={1020}
+        height={1000}
+        
+      />
+          </div>
+      </div>
+  </main>
+  </div></div>
+      {/* <img className="absolute left-0 top-0 w-[57rem] max-w-none rounded-md bg-white/5 ring-1 ring-white/10" src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png" alt="App screenshot" width="1824" height="1080"> */}
+    </div>
+  </div>
+</div>
+</div>
+
+
+
+    </div>
+
+  
     <main className="auth-page">
-      <SignUp />
-    </main>
+    <SignUp />
+  </main>
+  
+  <div className='conter '>
+  <div className="bg-blue ">
+<div className="mx-auto max-w-7xl px-6 lg:px-8">
+  
+  <div className="mx-auto mt-16 max-w-2xl rounded-3xl ring-1 ring-gray-200 sm:mt-20 lg:mx-0 lg:flex lg:max-w-none">
+    <div className="p-8 sm:p-10 lg:flex-auto">
+      <h3 className="text-2xl font-bold tracking-tight text-white-900">Lifetime membership</h3>
+      {/* <p className="mt-6 text-base leading-7 text--600">Lorem ipsum dolor sit amet consect etur adipisicing elit. Itaque amet indis perferendis blanditiis repellendus etur quidem assumenda.</p> */}
+      <div className="mt-10 flex items-center gap-x-4">
+        <h4 className="flex-none text-sm font-semibold leading-6 text--600">What’s included</h4>
+        <div className="h-px flex-auto '"></div>
+      </div>
+      <ul role="list" className="mt-8 grid grid-cols-1 gap-4 text-sm leading-6 text-gray-600 sm:grid-cols-2 sm:gap-6">
+        <li className="flex gap-x-3">
+          <svg className="h-6 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+            <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
+          </svg>
+          Private forum access
+        </li>
+        <li className="flex gap-x-3">
+          <svg className="h-6 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+            <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
+          </svg>
+          Member resources
+        </li>
+        <li className="flex gap-x-3">
+          <svg className="h-6 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+            <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
+          </svg>
+          Entry to annual conference
+        </li>
+        <li className="flex gap-x-3">
+          <svg className="h-6 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+            <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
+          </svg>
+          Official member 
+        </li>
+      </ul>
+    </div>
+    <div className="-mt-2 p-2 lg:mt-0 lg:w-full lg:max-w-md lg:flex-shrink-0">
+      <div className="rounded-2xl bg-gray-50 py-10 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:justify-center lg:py-16">
+        <div className="mx-auto max-w-xs px-8">
+          <p className="text-base font-semibold text-gray-600">Pay once, own it forever</p>
+          <p className="mt-6 flex items-baseline justify-center gap-x-2">
+            <span className="text-5xl font-bold tracking-tight text-gray-900">$0</span>
+            <span className="text-sm font-semibold leading-6 tracking-wide text-gray-600">USD</span>
+          </p>
+          <a href="#" className="mt-10 block w-full rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get access</a>
+          <p className="mt-6 text-xs leading-5 text-gray-600">Invoices and receipts available for easy.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+  </div>
+
+
+
+  <div>
+  
+
+<footer className="bg-blue rounded-lg shadow dark:bg-gray-900 m-4">
+  <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+      <div className="sm:flex sm:items-center sm:justify-between">
+          <a href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+              <img  />
+              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">CodeDocs</span>
+          </a>
+          <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-white-500 sm:mb-0 dark:text-white-400">
+              <li>
+                  <a href="#" className="hover:underline me-4 md:me-6">About</a>
+              </li>
+              <li>
+                  <a href="#" className="hover:underline me-4 md:me-6">Privacy Policy</a>
+              </li>
+              <li>
+                  <a href="#" className="hover:underline me-4 md:me-6">Licensing</a>
+              </li>
+              <li>
+                  <a href="#" className="hover:underline">Contact</a>
+              </li>
+          </ul>
+      </div>
+      <hr className="my-6 border-white-200 sm:mx-auto dark:border-white-700 lg:my-8" />
+      <span className="block text-sm text-white-500 sm:text-center dark:text-white-400">© 2024 <a href="" className="hover:underline">CodeDocs™</a>. All Rights Reserved.</span>
+  </div>
+</footer>
+
+
+  </div>
+
+  
+  </div>
   )
 }
 
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -19,6 +19,7 @@ const Home = async () => {
 
   return (
     <main className="home-container">
+    
       <Header className="sticky left-0 top-0">
         <div className="flex items-center gap-2 lg:gap-4">
           <Notifications />
